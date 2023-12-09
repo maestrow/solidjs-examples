2cc47b5bf6606b5518ee9bb98ce8c00e6baa05d4
maestrow <andrew.shmelev@gmail.com>
2023-12-09 16:26:39 +0300

    add comment

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,8 @@ function generateRandomString(length: number) {
 const books: string[] = ['111', '222', '333'];
 
 export const fetchBooks = () => {
-  return [...books]; //
+  // return books; - this wouldn't work
+  return [...books]; // Because solidjs will compare result by reference. And the books reference is the same array. So solid will decide that new data from refetch is the same and will not trigger update.
 };
 
 export const addBook = () => {
